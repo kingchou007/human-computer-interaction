36d5a67b17de84bdc762bdf0fbaa74dbd1871ecb
jli45@uvm.edu <jli45@uvm.edu>
2020-09-07 19:04:56 +0800

    Drawing with P5

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,5 +1,9 @@
 var controllerOptions = {} ;
-Leap.loop(controllerOptions, function(frame)
-{
-} 
+var i = 0;
+var x = 480, y = 438;
+Leap.loop(controllerOptions, function(frame){
+    console.log(i)
+    clear();
+    circle(Math.floor(Math.random() * 2) + 199, Math.floor(Math.random() * 2) + 199,100); 
+}
 );

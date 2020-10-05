edf6adcb6324102e062c0cfbfcef66d3060d4cf1
jli45@uvm.edu <jli45@uvm.edu>
2020-10-05 15:51:47 +0800

    Del05-step22 done

--- Record.js
+++ Record.js
@@ -1,64 +1,78 @@
 var controllerOptions = {};
-var oneFrameOfData = nj.zeros([5,4,6]);
-// var rawXMin = -10;
-// var rawXMax = 20;
-// var rawYMin = -10;
-// var rawYMax = 20;
+var x = window.innerWidth / 2;
+var y = window.innerHeight / 2;
+var z = 0;
+var x1 = window.innerWidth / 2;
+var y1 = window.innerHeight / 2;
+var z1 = 0;
+var x2 = window.innerWidth / 2;
+var y2 = window.innerHeight / 2;
+var z2 = 0;
 var previousNumHands = 0;
 var currentNumHands = 0;
-var moreHands;
+var moreThanOneHand;
 var numSamples = 2;
 var currentSample = 0;
 nj.config.printThreshold = 1000;
+var framesOfData = nj.zeros([5, 4, 6, numSamples]);
 
-Leap.loop(controllerOptions, function(frame){
-    currentNumHands = frame.hands.length;
-    clear();
-    HandleFrame(frame);
-    RecordData();
-    console.log();
-    previousNumHands = currentNumHands;
-});
+
+Leap.loop(controllerOptions, function(frame)
+    {
+        currentNumHands = frame.hands.length;
+        clear();
+        HandleFrame(frame);
+        RecordData();
+        previousNumHands = currentNumHands;
+    }
+);
 
 
 function HandleFrame(frame) {
     var interactionBox = frame.interactionBox;
-
     if (frame.hands.length == 1){
-        moreHands = false;
+        moreThanOneHand = false;
         var hand = frame.hands[0];
-        HandleHand(hand, moreHands, interactionBox);
+        HandleHand(hand, moreThanOneHand, interactionBox);
     } else if (frame.hands.length > 1) {
-        moreHands = true;
+        moreThanOneHand = true;
         var hand = frame.hands[0];
-        HandleHand(hand, moreHands, interactionBox);
+        HandleHand(hand, moreThanOneHand, interactionBox);
     } else {
-        moreHands = false;
+        moreThanOneHand = false;
     }
 }
 
 
-function HandleHand(hand, moreHands, interactionBox) {
+function HandleHand(hand, moreThanOneHand, interactionBox) {
     var fingers = hand.fingers;
     for (var i = 3; i >= 0; i -= 1) {
         for (var j = 4; j >= 0; j -= 1) {
-            HandleBone(fingers[j].bones[i], fingers[j].bones[i].type, j, moreHands, interactionBox);
+
+            HandleBone(fingers[j].bones[i], fingers[j].bones[i].type, j, moreThanOneHand, interactionBox);
         }
     }
 }
 
 
-function HandleBone(bone, boneType, fingerIndex, moreHands, interactionBox) {
-    var x1 = normalizedNextJoint[0];
-    var y1 = normalizedNextJoint[1];
-    var z1 = normalizedNextJoint[2];
-
-    var x2 = normalizedPrevJoint[0];
-    var y2 = normalizedPrevJoint[1];
-    var z2 = normalizedPrevJoint[2];
-
+function HandleBone(bone, boneType, fingerIndex, moreThanOneHand, interactionBox) {
     var normalizedPrevJoint = interactionBox.normalizePoint(bone.prevJoint, true);
     var normalizedNextJoint = interactionBox.normalizePoint(bone.nextJoint, true);
+
+    x1 = normalizedNextJoint[0];
+    y1 = normalizedNextJoint[1];
+    z1 = normalizedNextJoint[2];
+
+    x2 = normalizedPrevJoint[0];
+    y2 = normalizedPrevJoint[1];
+    z2 = normalizedPrevJoint[2];
+
+    framesOfData.set(fingerIndex, boneType, 0, currentSample, x1);
+    framesOfData.set(fingerIndex, boneType, 1, currentSample, y1);
+    framesOfData.set(fingerIndex, boneType, 2, currentSample, z1);
+    framesOfData.set(fingerIndex, boneType, 3, currentSample, x2);
+    framesOfData.set(fingerIndex, boneType, 4, currentSample, y2);
+    framesOfData.set(fingerIndex, boneType, 5, currentSample, z2);
 
     var canvasPrevX = window.innerWidth * normalizedPrevJoint[0];
     var canvasPrevY = window.innerHeight * (1 - normalizedPrevJoint[1]);
@@ -66,47 +80,33 @@ function HandleBone(bone, boneType, fingerIndex, moreHands, interactionBox) {
     var canvasNextX = window.innerWidth * normalizedNextJoint[0];
     var canvasNextY = window.innerHeight * (1 - normalizedNextJoint[1]);
 
-    // console.log(window.innerWidth + window.innerHeight);
-    // console.log( canvasPrevX + canvasPrevY);
-    // console.log( canvasNextX + canvasNextY);
-
-    // [x1, y1] = TransformCoordinates(x1, y1);
-    // [x2, y2] = TransformCoordinates(x2, y2);
-
-    frameOfData.set(fingerIndex, boneType, 0, x1);
-    frameOfData.set(fingerIndex, boneType, 1, y1);
-    frameOfData.set(fingerIndex, boneType, 2, z1);
-    frameOfData.set(fingerIndex, boneType, 3, x2);
-    frameOfData.set(fingerIndex, boneType, 4, y2);
-    frameOfData.set(fingerIndex, boneType, 5, z2);
-
-    if (moreHands) {
+    if (moreThanOneHand) {
         if (boneType == 0) {
-            strokeWeight(12);
-            stroke(250,0,0);
+            strokeWeight(8*3);
+            stroke(255,0,0);
         } else if (boneType == 1) {
-            strokeWeight(8);
-            stroke(160,0,0);
+            strokeWeight(6*3);
+            stroke(207,0,0);
         } else if (boneType == 2) {
-            strokeWeight(5);
-            stroke(110,0,0);
+            strokeWeight(4*3);
+            stroke(158,0,0);
         } else {
-            strokeWeight(2);
-            stroke(50,0,0);
+            strokeWeight(2*3);
+            stroke(115,0,0);
         }
     } else {
         if (boneType == 0) {
-            strokeWeight(12);
-            stroke(0,250,0);
+            strokeWeight(8*3);
+            stroke(0,255,0);
         } else if (boneType == 1) {
-            strokeWeight(8);
-            stroke(0,160,0);
+            strokeWeight(6*3);
+            stroke(0,207,0);
         } else if (boneType == 2) {
-            strokeWeight(5);
-            stroke(0,110,0);
+            strokeWeight(4*3);
+            stroke(0,158,0);
         } else {
-            strokeWeight(2);
-            stroke(0,50,0);
+            strokeWeight(2*3);
+            stroke(0,115,0);
         }
     }
     line(canvasNextX, canvasNextY, canvasPrevX, canvasPrevY, z1, z2);
@@ -114,16 +114,17 @@ function HandleBone(bone, boneType, fingerIndex, moreHands, interactionBox) {
 
 
 function RecordData() {
-    if (previousNumHands == 1 && currentNumHands == 2) {
-        currentSample++;
-        if (currentSample >= numSamples) {
+    if (currentNumHands == 2) {
+        currentSample += 1;
+
+        if (currentSample == numSamples) {
             currentSample = 0;
         }
     }
+
     if (previousNumHands == 2 && currentNumHands == 1) {
         background(0)
         console.log(framesOfData.toString());
-        console.log(currentSample);
     }
 }
 

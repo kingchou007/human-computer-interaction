c8a858f23b068642a75d2368dcc3037ce1f2da80
jli45@uvm.edu <jli45@uvm.edu>
2020-10-12 18:30:47 +0800

    Del_05 step4

--- PredictGestures.js
+++ PredictGestures.js
@@ -4,6 +4,7 @@ var numSamples = 2;
 var testingSampleIndex = 0;
 // var predictedClassLabels = nj.zeros(2);
 var controllerOptions;
+var moreThanOneHand;
 
 // function draw(){
 Leap.loop(controllerOptions, function(frame){
@@ -11,9 +12,11 @@ Leap.loop(controllerOptions, function(frame){
     if (trainingCompleted == false){
         Train();
     }
+    HandleFrame(frame);
     Test();
 });
 
+// Train 0 and 2
 function Train(){
     trainingCompleted = true;
     for (var i = 0; i < train0.shape[3]; i++) {
@@ -43,32 +46,89 @@ function GotResults(err, result){
 }
 
 function DrawCircles(){
-    for (var j = 0; j < numSamples; j++) {
-        var x = irisData.pick(j).get(0);
-        var y = irisData.pick(j).get(1);
-        var c = irisData.pick(j).get(4);
+}
 
-        if (c == 0){
-            fill('rgb(250,0,0)');
-        } else if (c == 1){
-            fill('rgb(0,235,0)');
-        } else {
-            fill('rgb(0,0,250)');
-        }
 
-        if (j % 2 == 0){
-            stroke('rgb(0,0,0)');
-        } else {
-            console.log(j + ": " + predictedClassLabels.get(j));
-            if (predictedClassLabels.get(j) == 0){
-                stroke('rgb(250,0,0)');
-            } else if (predictedClassLabels.get(j) == 1){
-                stroke('rgb(0,235,0)');
-            } else {
-                stroke('rgb(0,0,250)');
-            }
-        }
-
-        circle(x*100,y*100,10);
+function HandleFrame(frame) {
+    var interactionBox = frame.interactionBox;
+    if (frame.hands.length == 1){
+        moreThanOneHand = false;
+        var hand = frame.hands[0];
+        HandleHand(hand, moreThanOneHand, interactionBox);
+    } else if (frame.hands.length > 1) {
+        moreThanOneHand = true;
+        var hand = frame.hands[0];
+        HandleHand(hand, moreThanOneHand, interactionBox);
+    } else {
+        moreThanOneHand = false;
     }
+}
+
+
+function HandleHand(hand, moreThanOneHand, interactionBox) {
+    var fingers = hand.fingers;
+    for (var i = 3; i >= 0; i -= 1) {
+        for (var j = 4; j >= 0; j -= 1) {
+
+            HandleBone(fingers[j].bones[i], fingers[j].bones[i].type, j, moreThanOneHand, interactionBox);
+        }
+    }
+}
+
+
+function HandleBone(bone, boneType, fingerIndex, moreThanOneHand, interactionBox) {
+    var normalizedPrevJoint = interactionBox.normalizePoint(bone.prevJoint, true);
+    var normalizedNextJoint = interactionBox.normalizePoint(bone.nextJoint, true);
+
+    x1 = normalizedNextJoint[0];
+    y1 = normalizedNextJoint[1];
+    z1 = normalizedNextJoint[2];
+
+    x2 = normalizedPrevJoint[0];
+    y2 = normalizedPrevJoint[1];
+    z2 = normalizedPrevJoint[2];
+
+    framesOfData.set(fingerIndex, boneType, 0, currentSample, x1);
+    framesOfData.set(fingerIndex, boneType, 1, currentSample, y1);
+    framesOfData.set(fingerIndex, boneType, 2, currentSample, z1);
+    framesOfData.set(fingerIndex, boneType, 3, currentSample, x2);
+    framesOfData.set(fingerIndex, boneType, 4, currentSample, y2);
+    framesOfData.set(fingerIndex, boneType, 5, currentSample, z2);
+
+    var canvasPrevX = window.innerWidth * normalizedPrevJoint[0];
+    var canvasPrevY = window.innerHeight * (1 - normalizedPrevJoint[1]);
+
+    var canvasNextX = window.innerWidth * normalizedNextJoint[0];
+    var canvasNextY = window.innerHeight * (1 - normalizedNextJoint[1]);
+
+    if (moreThanOneHand) {
+        if (boneType == 0) {
+            strokeWeight(12*3);
+            stroke(255,0,0);
+        } else if (boneType == 1) {
+            strokeWeight(8*3);
+            stroke(207,0,0);
+        } else if (boneType == 2) {
+            strokeWeight(5*3);
+            stroke(158,0,0);
+        } else {
+            strokeWeight(2*3);
+            stroke(0,0,0);
+        }
+    } else {
+        if (boneType == 0) {
+            strokeWeight(12*3);
+            stroke(0,255,0);
+        } else if (boneType == 1) {
+            strokeWeight(8*3);
+            stroke(0,207,0);
+        } else if (boneType == 2) {
+            strokeWeight(5*3);
+            stroke(0,158,0);
+        } else {
+            strokeWeight(2*3);
+            stroke(0,0,0);
+        }
+    }
+    line(canvasNextX, canvasNextY, canvasPrevX, canvasPrevY, z1, z2);
 }

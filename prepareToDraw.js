645a2a4eb47ee83be66032af5f9ff7d67ea4bd5f
jli45@uvm.edu <jli45@uvm.edu>
2020-11-02 22:07:26 +0800

    Del_07 Done

--- prepareToDraw.js
+++ prepareToDraw.js
@@ -1,3 +1,18 @@
-function setup() { 
+var img;
+var arrowAway;
+var arrowTowards;
+var arrowRight;
+var arrowLeft;
+var arrowDown;
+var arrowUp;
+
+function setup() {
     createCanvas(window.innerWidth,window.innerHeight);
+    img = loadImage('https://i.imgur.com/xlgHoq9.jpg');
+    arrowAway = loadImage('https://i.imgur.com/ArRz2qu.jpg');
+    arrowTowards  = loadImage('https://i.imgur.com/jemJrwT.jpg');
+    arrowRight = loadImage('https://i.imgur.com/Yy9SwoG.jpg');
+    arrowLeft = loadImage('https://i.imgur.com/YFXuaXu.jpg');
+    arrowDown = loadImage('https://i.imgur.com/6gNfGbL.jpg');
+    arrowUp = loadImage('https://i.imgur.com/7MedbAa.jpg');
 }

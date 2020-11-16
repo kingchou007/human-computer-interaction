02f30dd83958ecfb0a1964a3bbfe16642b710a15
jli45@uvm.edu <jli45@uvm.edu>
2020-11-16 17:53:32 +0800

    Del_10 ASL number(1-10)

--- prepareToDraw.js
+++ prepareToDraw.js
@@ -5,10 +5,16 @@ var arrowRight;
 var arrowLeft;
 var arrowDown;
 var arrowUp;
-
-// create two new variables that point to those two images. (Del_09)
 var n0;
+var n1;
 var n2;
+var n3;
+var n4;
+var n5;
+var n6;
+var n7;
+var n8;
+var n9;
 
 function setup() {
     createCanvas(window.innerWidth,window.innerHeight);
@@ -20,5 +26,13 @@ function setup() {
     arrowDown = loadImage('https://i.imgur.com/6gNfGbL.jpg');
     arrowUp = loadImage('https://i.imgur.com/7MedbAa.jpg');
     n0 = loadImage('https://i.imgur.com/OzbNmdk.png');
+    n1 = loadImage('https://i.imgur.com/FjkAssp.png');
     n2 = loadImage('https://i.imgur.com/BXG9Zs7.png');
+    n3 = loadImage('https://i.imgur.com/n1GjXjQ.png');
+    n4 = loadImage('https://i.imgur.com/5bfPRkh.png');
+    n5 = loadImage('https://i.imgur.com/NNsqR9L.png');
+    n6 = loadImage('https://i.imgur.com/FfC58OZ.png');
+    n7 = loadImage('https://i.imgur.com/584BI6a.png');
+    n8 = loadImage('https://i.imgur.com/oRvjudV.png');
+    n9 = loadImage('https://i.imgur.com/Uko99bk.png');
 }
